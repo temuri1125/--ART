b9b2b0cbf9369810c63156e64efe3c71ac3cc5b3
temuri1125 <temuri.kitoshvili@gmail.com>
2019-08-16 22:11:50 +0400

    slidedown

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+// ჩამოსქროლვის დაწყება
 $(document).ready(function() {
-  $("#img1").click(function() {
-    $("#page2").toggle(500)
-  });
-});
+	$('#img1').click(function(){
+		$('#page2').slideToggle("slow", function(){
+		    if($('#page2').is(":visible")){
+		        $("html, body").animate({scrollTop: $('#page2').offset().top},300);
+		    }
+		});
+	});
+})
+
+// ჩამოსქროლვის დასრულება
